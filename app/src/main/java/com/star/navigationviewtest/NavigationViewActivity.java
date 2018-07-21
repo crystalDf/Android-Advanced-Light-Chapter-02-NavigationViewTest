8a691056ea016de17a7932b547e9183b3cd97754
crystalDf <chendong333@gmail.com>
2018-07-21 23:43:51 +0800

    NavigationViewTest

---
+++ app/src/main/java/com/star/navigationviewtest/NavigationViewActivity.java
@@ -0,0 +1,13 @@
+package com.star.navigationviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigationview);
+    }
+}
