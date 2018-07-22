eb74b80ebb2171f4d6e9e379ebff12e9fce05fcb
crystalDf <chendong333@gmail.com>
2018-07-22 22:51:29 +0800

    NavigationViewTest

--- app/src/main/java/com/star/navigationviewtest/NavigationViewActivity.java
+++ app/src/main/java/com/star/navigationviewtest/NavigationViewActivity.java
@@ -1,13 +1,67 @@
 package com.star.navigationviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NavigationViewActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private Toolbar mToolbar;
+
+    private FragmentAdapter mFragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigationview);
+        setContentView(R.layout.activity_navigation_view);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mViewPager = findViewById(R.id.viewpager);
+
+        initViewPager();
+    }
+
+    private void initViewPager() {
+
+        mTabLayout = findViewById(R.id.tab_layout);
+
+        List<String> titles = new ArrayList<>();
+        List<Fragment> fragments = new ArrayList<>();
+
+        titles.add("精选");
+        titles.add("体育");
+        titles.add("巴萨");
+        titles.add("购物");
+        titles.add("明星");
+        titles.add("视频");
+        titles.add("健康");
+        titles.add("励志");
+        titles.add("图文");
+        titles.add("本地");
+        titles.add("动漫");
+        titles.add("搞笑");
+        titles.add("精选");
+
+        for (String title : titles) {
+
+            mTabLayout.addTab(mTabLayout.newTab().setText(title));
+            fragments.add(new ListFragment());
+        }
+
+        mFragmentAdapter = new FragmentAdapter(
+                getSupportFragmentManager(), fragments, titles);
+
+        mViewPager.setAdapter(mFragmentAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/com/star/navigationviewtest/RecyclerViewAdapter.java
@@ -0,0 +1,51 @@
+package com.star.navigationviewtest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class RecyclerViewAdapter extends
+        RecyclerView.Adapter<RecyclerViewAdapter.RecyclerViewHolder> {
+
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+
+        return new RecyclerViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+
+        View view = holder.itemView;
+
+        view.setOnClickListener(v ->
+                Toast.makeText(mContext, "奔跑在孤傲的路上", Toast.LENGTH_LONG).show());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+        public RecyclerViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
