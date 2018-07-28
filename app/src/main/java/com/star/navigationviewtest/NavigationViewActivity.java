6a96279f955400551c61ef1afce631f7d05ae714
crystalDf <chendong333@gmail.com>
2018-07-28 21:58:10 +0800

    NavigationViewTest

--- app/src/main/java/com/star/navigationviewtest/NavigationViewActivity.java
+++ app/src/main/java/com/star/navigationviewtest/NavigationViewActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -93,5 +95,18 @@ public class NavigationViewActivity extends AppCompatActivity {
 
         mViewPager.setAdapter(mFragmentAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
